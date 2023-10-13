f85fe1665f49be45793a1abb8bb7f05ad6404e5e
codeme254 <otwomadennis365@gmail.com>
2023-10-13 10:51:41 +0300

    prettify

--- api/index.js
+++ api/index.js
@@ -1,17 +1,20 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log("connected to database")
-}).catch((err) => {
-    console.log("Error connecting to the database")
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to the database");
+    console.log(err);
+  });
 
-const app = express()
+const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+  console.log("Server is running on port 3000");
+});
