41d3de36c223bedf8aca24826fd89ae2262edc3d
codeme254 <otwomadennis365@gmail.com>
2023-10-14 21:50:12 +0300

    complete the sign up logic

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,6 +3,9 @@ import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  if (!password) {
+    return errorHandler(404, "Password is required")
+  }
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
